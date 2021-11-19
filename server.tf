709ac477ecf3f90001df4624bf16a957d43c4168
praveencheruku <94554433+praveencheruku@users.noreply.github.com>
2021-11-19 19:25:53 +0530

    Update server.tf

--- server.tf
+++ server.tf
@@ -3,12 +3,12 @@ region = "us-east-2"
 }
 resource "aws_instance" "myawsserver" {
   ami = "ami-077e31c4939f6a2f3"
-  instance_type = "t2.micro"
+  instance_type = "t2.small"
   key_name = "cpk-import"
 
   tags = {
-    Name = "praveen-devops-ec2-instance-v2"
-    env = "development"
+    Name = "praveen-devsecops-ec2-instance-v2"
+    env = "production"
   }
   provisioner "local-exec" {
     command = "echo The servers IP address is ${self.public_ip} && echo ${self.public_ip} > /root/inv"
